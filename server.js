d368a3eae71fba0aabd956442086f844b897c0e2
Michael Flor <mflor54@gmail.com>
2020-08-15 15:39:04 -0400

    changed port to 80 for droplet server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const scraper = require('./pokemonScraper');
 
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = 80;
 
 const server = http.createServer(async (req, res) => {
     const headers = {
